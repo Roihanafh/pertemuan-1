febdb62eb8d7fb7a716acbd27a1441cbf5eec656
Roihanafh <roihanalfahmi123@gmail.com>
2023-11-01 20:45:13 +0700

    print mars

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.out.println("Hello earth!");
+        System.out.println("Hello mars!");
     }
 }
